1fe57638d7cb286e62696cc2d9a9bde8abf2eb23
Arjun-G-04 <arjungirirajkumar@gmail.com>
2025-05-23 21:48:55 +0530

    fix: meta data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,19 +7,17 @@
 <svelte:head>
 	<title>Designers' Consortium</title>
 	<meta name="description" content="Defy. Design. Deliver." />
-  
-	<link rel="icon" href="/logo.png" />
 
 	<meta property="og:title" content="Designers' Consortium" />
 	<meta property="og:description" content="Defy. Design. Deliver." />
-	<meta property="og:image" content="/bg-logo.webp" />
+	<meta property="og:image" content="/big-logo.webp" />
 	<meta property="og:url" content="https://dc.nitt.edu" />
 	<meta property="og:type" content="website" />
 
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content="Designers' Consortium" />
 	<meta name="twitter:description" content="Defy. Design. Deliver." />
-	<meta name="twitter:image" content="/bg-logo.webp" />
+	<meta name="twitter:image" content="/big-logo.webp" />
 </svelte:head>
 
 {@render children()}
